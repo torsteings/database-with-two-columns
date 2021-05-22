376725b1df1c44b9039e48c45d7cc14eae3f1c4b
ctbirds <ctbirdsas@gmail.com>
2021-05-22 19:13:35 +0200

    first commit

---
+++ scraping/management/commands/scrape.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+
+from scraping.models import Job
+class Command(BaseCommand):
+    # define logic of command
+    def handle(self, *args, **options):
+        try:
+            # save in db
+            Job.objects.create(
+            url="test",            #insert comma if several columns
+            open_price = 10.05
+            )
+        except:
+            print('error')
+
+        self.stdout.write( 'job complete' )
